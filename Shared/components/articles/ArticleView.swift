5267abdac7f515428bddf842b9bc6243e34d5fa8
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-04-04 18:11:31 +0200

    - extracts BookmarkView into a dedicated files and views

--- Shared/components/articles/ArticleView.swift
+++ Shared/components/articles/ArticleView.swift
@@ -20,36 +20,10 @@ struct Actions: View {
   
   var body: some View {
     HStack {
-      Bookmark(article: article)
+      BookmarkView(article: article)
       Link(destination: article.link) {
         Image(systemName: "safari").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-      }
-    }.font(.system(size: 25))
-  }
-}
-
-struct Bookmark: View {
-  let article: Article
-  @EnvironmentObject var model: ArticlesViewModel
-  
-  var body: some View {
-    Button(action: { withAnimation(.spring(response: 0.45, dampingFraction: article.bookmarked ? 0.825 : 0.45)) { model.toggleBookmark(article) } }) {
-      ZStack {
-        Image(systemName: "bookmark.fill")
-          .font(.system(size: 13))
-          .colorMultiply(article.bookmarked ? .purple : .black)
-        Image(systemName: "circle")
-          .font(.system(size: 25))
-          .colorMultiply(article.bookmarked ? .purple : .black)
-          .overlay(
-            Color.purple
-              .opacity(0.15)
-              .clipShape(Circle())
-              .padding(3.5)
-          )
-          .scaleEffect(article.bookmarked ? 1 : 0)
-        
-      }.foregroundColor(.white)
+      }.font(.system(size: 25))
     }
   }
 }
---
+++ Shared/components/articles/BookmarkView.swift
@@ -0,0 +1,79 @@
+//
+//  BookmarkView.swift
+//  dev-articles
+//
+//  Created by Mathias Remshardt on 04.04.21.
+//
+
+import SwiftUI
+
+struct BookmarkView: View {
+  let article: Article
+  let size: CGFloat = 25
+  @EnvironmentObject var model: ArticlesViewModel
+  
+  var body: some View {
+    Button(action: { withAnimation(.spring(response: 0.45, dampingFraction: article.bookmarked ? 0.825 : 0.45)) { model.toggleBookmark(article) } }) {
+      ZStack {
+        Bookmark(bookmarked: article.bookmarked, size: size)
+        BCircle(bookmarked: article.bookmarked)
+        
+      }
+      .font(.system(size: size))
+      .foregroundColor(.white)
+    }
+  }
+}
+
+struct Bookmark: View {
+  let bookmarked: Bool
+  let size: CGFloat
+  
+  var body: some View {
+    Image(systemName: "bookmark.fill")
+      .font(.system(size: size * 0.52))
+      .colorMultiply(bookmarked ? .purple : .black)
+  }
+}
+
+struct BCircle: View {
+  let bookmarked: Bool
+  
+  var body: some View {
+    Image(systemName: "circle")
+      .colorMultiply(bookmarked ? .purple : .black)
+      .overlay(
+        Color.purple
+          .opacity(0.15)
+          .clipShape(Circle())
+          .padding(3.5)
+      )
+      .scaleEffect(bookmarked ? 1 : 0)
+  }
+}
+
+#if DEBUG
+struct Bookmark_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      BookmarkView(
+        article: Article(
+          title: "Other Article",
+          id: 1,
+          description: "Other Article",
+          link: URL(string: "https://dev.to/remshams/rolling-up-a-multi-module-system-esm-cjs-compatible-npm-library-with-typescript-and-babel-3gjg")!)
+      )
+      
+      BookmarkView(
+        article: Article(
+          title: "Other Article",
+          id: 1,
+          description: "Other Article",
+          link: URL(string: "https://dev.to/remshams/rolling-up-a-multi-module-system-esm-cjs-compatible-npm-library-with-typescript-and-babel-3gjg")!, bookmarked: true)
+      )
+    }.previewLayout(.sizeThatFits)
+    
+  }
+}
+
+#endif
