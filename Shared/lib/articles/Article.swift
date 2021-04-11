6bfcf3b09d934fea8029b8cd9a525ebf1f26949f
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-04-11 13:13:20 +0200

    - moves list article definition to article

--- Shared/lib/articles/Article.swift
+++ Shared/lib/articles/Article.swift
@@ -1,6 +1,19 @@
 import Foundation
+import Combine
 
 typealias ArticleId = Int
+
+enum TimeCategory {
+  case feed
+  case day
+  case week
+  case month
+  case year
+}
+
+protocol ListArticle {
+  func list$(for timeCategory: TimeCategory) -> AnyPublisher<[Article], RestError>
+}
 
 struct Article: Identifiable, Equatable {
   let title: String
