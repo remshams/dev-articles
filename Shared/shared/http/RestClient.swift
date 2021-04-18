c8c09eb7587abeb7d818af0cc8c0b1aeabb65352
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-04-18 13:08:20 +0200

    - restructures tests to domain structure

--- Shared/shared/http/RestClient.swift
+++ Shared/shared/http/RestClient.swift
@@ -2,3 +2,14 @@ import Combine
 import Foundation
 
 struct RestHttpClient: HttpGet {}
+
+extension RestHttpClient {
+  func get(for url: URL) -> AnyPublisher<Data, HttpError> {
+    URLSession.shared.dataTaskPublisher(for: url)
+      .map(\.data)
+      .mapError() { error in
+        HttpError.serverError
+      }
+      .eraseToAnyPublisher()
+  }
+}
