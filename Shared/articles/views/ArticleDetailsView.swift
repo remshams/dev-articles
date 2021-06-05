996cb2a41a75d36429db35b2f566b6170cf58d1e
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-06-05 17:02:43 +0200

    * adds placeholders for ArticleDetailsView

--- Shared/articles/views/ArticleDetailsView.swift
+++ Shared/articles/views/ArticleDetailsView.swift
@@ -12,8 +12,48 @@ struct ArticleDetailsView: View {
   let article: Article
 
   var body: some View {
-    HStack {
-      Text(article.title)
+    GeometryReader { gp in
+      VStack(spacing: 0) {
+        CoverImage().zIndex(1)
+          .frame(height: gp.size.height * 1 / 3)
+        Details()
+      }.background(Color.gray.opacity(0.1))
     }
   }
 }
+
+private struct CoverImage: View {
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      Rectangle().foregroundColor(.blue)
+      ProfileImage()
+        .frame(width: 140, height: 140)
+        .offset(CGSize(width: 0, height: 35))
+    }
+    
+  }
+}
+
+private struct ProfileImage: View {
+  var body: some View {
+    Circle()
+      .padding(4)
+      .foregroundColor(.gray)
+      .background(Color.white)
+      .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+  }
+}
+
+private struct Details: View {
+  var body: some View {
+    Rectangle().foregroundColor(.clear)
+  }
+}
+
+#if DEBUG
+  struct ArticleDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+      ArticleDetailsView(article: articleForPreview)
+    }
+  }
+#endif
