e17cd8749ea7beaf818ae6c1898a32da1f7ca3f6
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-06-20 15:14:48 +0200

    * adds ArticleContentView stub

--- Shared/articles/views/ArticleContentView.swift
+++ Shared/articles/views/ArticleContentView.swift
@@ -5,11 +5,30 @@
 //  Created by Mathias Remshardt on 19.06.21.
 //
 
+import Combine
 import Foundation
 import SwiftUI
+import WebKit
+
+struct Details: Codable {
+  // swiftlint:disable:next all
+  let body_html: String
+}
 
 struct ArticleContentView: View {
   var body: some View {
-    Text("ArticleContent")
+    Text("Text")
+  }
+}
+
+private struct WebView: UIViewRepresentable {
+  let text: String
+
+  func makeUIView(context _: Context) -> WKWebView {
+    return WKWebView()
+  }
+
+  func updateUIView(_ uiView: WKWebView, context _: Context) {
+    uiView.loadHTMLString(text, baseURL: nil)
   }
 }
