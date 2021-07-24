8ddf572df841ab4dd2cdedea55359e14b6b081f0
Mathias Remshardt <remshams@gmail.com>
2021-07-24 18:37:11 +0200

    feat(ArticleByLink): tab navigation
    * splits entry views for iOS and macOS
    * adds tab navigation to iOS

---
+++ iOS/AppView.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  dev-articles
+//
+//  Created by Mathias Remshardt on 24.07.21.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct AppView: App {
+  let appContainer: AppContainer
+
+  init() {
+    appContainer = AppContainer()
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      TabView {
+        ArticlesView()
+          .tabItem {
+            Image(systemName: "book")
+            Text("Articles")
+          }
+        Text("ReadingList")
+          .tabItem {
+            Image(systemName: "bookmark")
+            Text("Readinglist")
+          }
+      }
+      .environmentObject(appContainer.makeArticlesContainer())
+      .environmentObject(appContainer.makeReadingListContainer())
+    }
+  }
+}
