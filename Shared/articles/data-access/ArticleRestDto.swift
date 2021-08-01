5030ddf6bb5e27c9f853e55ea50ee20b39f73eb7
Mathias Remshardt <remshams@gmail.com>
2021-08-01 18:48:42 +0200

    feat(AddArticle): implements workaround for "tags"/"tag_list" issue
    Property types are different when requesting article list and a single article

--- Shared/articles/data-access/ArticleRestDto.swift
+++ Shared/articles/data-access/ArticleRestDto.swift
@@ -21,7 +21,72 @@ struct ArticleRestDto: Identifiable, Codable {
   let public_reactions_count: Int
   let user: AuthorRestDto
   let tag_list: [String]
+  let tags: String
+
+  init(
+    id: Int,
+    title: String,
+    description: String,
+    url: String,
+    cover_image: String?,
+    published_at: String,
+    reading_time_minutes: Int,
+    comments_count: Int,
+    positive_reactions_count: Int,
+    public_reactions_count: Int,
+    user: AuthorRestDto,
+    tag_list: [String]
+  ) {
+    self.id = id
+    self.title = title
+    self.description = description
+    self.url = url
+    self.cover_image = cover_image
+    self.published_at = published_at
+    self.reading_time_minutes = reading_time_minutes
+    self.comments_count = comments_count
+    self.positive_reactions_count = positive_reactions_count
+    self.public_reactions_count = public_reactions_count
+    self.user = user
+    self.tag_list = tag_list
+    self.tags = tag_list.joined(separator: ",")
+  }
+
   // swiftlint:enable identifier_name
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: ArticleRestDto.CodingKeys.self)
+
+    id = try container.decode(Int.self, forKey: ArticleRestDto.CodingKeys.id)
+    title = try container.decode(String.self, forKey: ArticleRestDto.CodingKeys.title)
+    description = try container.decode(String.self, forKey: ArticleRestDto.CodingKeys.description)
+    url = try container.decode(String.self, forKey: ArticleRestDto.CodingKeys.url)
+    cover_image = try container.decode(String?.self, forKey: ArticleRestDto.CodingKeys.cover_image)
+    published_at = try container.decode(String.self, forKey: ArticleRestDto.CodingKeys.published_at)
+    reading_time_minutes = try container.decode(Int.self, forKey: ArticleRestDto.CodingKeys.reading_time_minutes)
+    comments_count = try container.decode(Int.self, forKey: ArticleRestDto.CodingKeys.comments_count)
+    positive_reactions_count = try container.decode(
+      Int.self,
+      forKey: ArticleRestDto.CodingKeys.positive_reactions_count
+    )
+    public_reactions_count = try container.decode(Int.self, forKey: ArticleRestDto.CodingKeys.public_reactions_count)
+    user = try container.decode(AuthorRestDto.self, forKey: ArticleRestDto.CodingKeys.user)
+    // Workaround for issue https://github.com/forem/forem/issues/14396
+    // "tags" and "tag_list" types can be switched so therefor in case decoding of "tag_list" fails "tags" is tried.
+    // In case this fails as well the "tag_list" is set to an empty array.
+    do {
+      tag_list = try container.decode([String].self, forKey: ArticleRestDto.CodingKeys.tag_list)
+      tags = tag_list.joined(separator: ",")
+    } catch {
+      do {
+        tag_list = try container.decode([String].self, forKey: ArticleRestDto.CodingKeys.tags)
+        tags = tag_list.joined(separator: ",")
+      } catch {
+        tag_list = []
+        tags = ""
+      }
+    }
+  }
 }
 
 // MARK: Extensions
