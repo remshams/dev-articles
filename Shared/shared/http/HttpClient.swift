8e563bd5b063c06e55fc52d0500531a65fb7fe58
Mathias Remshardt <remshams@gmail.com>
2021-08-02 19:44:12 +0200

    feat(AddArticle): refactors logDecodingError
    Renames logDecodingError to logAndMapDEcodingErrorr as it, next to doing some logging, maps decoding
    errors to HttpError.serverError.
    Moves publisher extensions in operators to HttpClient.

--- Shared/shared/http/HttpClient.swift
+++ Shared/shared/http/HttpClient.swift
@@ -1,5 +1,6 @@
 import Combine
 import Foundation
+import OSLog
 
 protocol HttpGet {
   func get(for url: URL, receiveOn queue: DispatchQueue) -> AnyPublisher<Data, HttpError>
@@ -8,5 +9,42 @@ protocol HttpGet {
 extension HttpGet {
   func get(for url: URL) -> AnyPublisher<Data, HttpError> {
     get(for: url, receiveOn: DispatchQueue.main)
+  }
+}
+
+extension Publisher where Output == Data {
+  func decode<Model: Decodable>(
+    to type: Model.Type = Model.self,
+    decoder: JSONDecoder = .init()
+  ) -> AnyPublisher<Model, HttpError> {
+    decode(type: type, decoder: decoder)
+      .logAndMapDecodingError()
+      .eraseToAnyPublisher()
+  }
+}
+
+extension Publisher {
+  func logAndMapDecodingError() -> Publishers.MapError<Self, HttpError> {
+    mapError { error in
+      if let error = error as? DecodingError {
+        switch error {
+        case let .keyNotFound(key, context):
+          Logger().debug("\(key.stringValue) not found: \(context.debugDescription)")
+        case let .typeMismatch(_, context):
+          Logger().debug("\(context.codingPath) not matching: \(context.debugDescription)")
+        case let .valueNotFound(_, context):
+          Logger().debug("\(context.codingPath) not found: \(context.debugDescription)")
+        case let .dataCorrupted(context):
+          Logger().debug("Invalid data: \(context.debugDescription)")
+        default:
+          Logger().debug("Decoding error")
+        }
+      }
+      if let error = error as? HttpError {
+        return error
+      } else {
+        return .serverError
+      }
+    }
   }
 }
