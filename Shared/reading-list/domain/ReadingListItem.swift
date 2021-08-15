f6bdf71e60cd22e93860425fae5d90d5e9c8cbf4
Mathias Remshardt <remshams@gmail.com>
2021-08-15 18:00:46 +0200

    feat(ReadingList): renames ReadingListItem domain model

--- Shared/reading-list/domain/ReadingListItem.swift
+++ Shared/reading-list/domain/ReadingListItem.swift
@@ -1,5 +1,5 @@
 //
-//  ReadingListItemDbDto+CoreDataProperties.swift
+//  ReadingListItemDbDto+CoreDataClass.swift
 //  dev-articles (iOS)
 //
 //  Created by Mathias Remshardt on 21.03.21.
@@ -9,17 +9,25 @@
 import CoreData
 import Foundation
 
-extension ReadingListItem {
+@objc(ReadingListItem)
+public class ReadingListItem: NSManagedObject {
+  @NSManaged private(set) var title: String
+  @NSManaged private(set) var contentId: String
+  @NSManaged private(set) var savedAt: Date
+
+  convenience init(context: NSManagedObjectContext, title: String, contentId: String, savedAt: Date = Date()) {
+    self.init(context: context)
+    self.title = title
+    self.contentId = contentId
+    self.savedAt = savedAt
+  }
+}
+
+extension ReadingListItem: Identifiable {
   @nonobjc class func fetchRequestAll() -> NSFetchRequest<ReadingListItem> {
     NSFetchRequest<ReadingListItem>(entityName: "ReadingListItem")
   }
 
-  @NSManaged private(set) var title: String
-  @NSManaged private(set) var contentId: String
-  @NSManaged private(set) var savedAt: Date
-}
-
-extension ReadingListItem {
   @nonobjc class func fetchRequest(predicate: NSPredicate) -> NSFetchRequest<ReadingListItem> {
     let fetchRequest = NSFetchRequest<ReadingListItem>(entityName: "ReadingListItem")
     fetchRequest.predicate = predicate
@@ -30,14 +38,5 @@ extension ReadingListItem {
     let fetchRequest = NSFetchRequest<ReadingListItem>(entityName: "ReadingListItem")
     fetchRequest.predicate = NSPredicate(format: "contentId IN %@", articleIds.map { String($0) })
     return fetchRequest
-  }
-}
-
-extension ReadingListItem: Identifiable {
-  convenience init(context: NSManagedObjectContext, title: String, contentId: String, savedAt: Date = Date()) {
-    self.init(context: context)
-    self.title = title
-    self.contentId = contentId
-    self.savedAt = savedAt
   }
 }
