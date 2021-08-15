705d3074cb00cbb23b6b45c4eab42f35efd01a2e
Mathias Remshardt <remshams@gmail.com>
2021-08-15 13:03:48 +0200

    feat(ReadingListView): updates adding of reading list item
    Now uses core data to check for existing readinglist item

--- Shared/reading-list/view/ReadingListView.swift
+++ Shared/reading-list/view/ReadingListView.swift
@@ -49,7 +49,7 @@ private struct ContainerView: View {
       }
       .sheet(isPresented: $showAddArticle) {
         AddArticleView { article in
-          model.add(article: article, readingListItems: allArticles)
+          model.add(article: article)
           showAddArticle = false
           selectedArticle = article.id
         } cancelAddArticle: { showAddArticle = false }
--- Tests Shared/reading-list/views/ReadingListViewModelTests.swift
+++ Tests Shared/reading-list/views/ReadingListViewModelTests.swift
@@ -7,37 +7,39 @@
 
 @testable import dev_articles
 import Foundation
+import SwiftUI
 import XCTest
+import CoreData
 
 class ReadingListViewModelTests: XCTestCase {
   var article: Article!
   var model: ReadingListViewModel!
+  var context: NSManagedObjectContext!
 
   override func setUp() {
     AppContainer.shared.persistence.context.reset()
     article = Article.createFixture()
     model = ReadingListViewModel()
+    context = AppContainer.shared.persistence.context
   }
 
   func test_add_shouldAddArticleToList() throws {
     model.add(article: article)
 
-    XCTAssertEqual(model.bookmarkedArticles.count, 1)
-    XCTAssertEqual(model.bookmarkedArticles[0].article, BookmarkedArticle.from(article: article).article)
-    let readingListItems = try AppContainer.shared.persistence.context
-      .fetch(ReadingListItem.fetchRequestAll())
-    XCTAssertEqual(readingListItems.count, 1)
+    let readingListItems = try context.fetch(ReadingListItem.fetchRequest(articleIds: [article.id]))
+    XCTAssertFalse(readingListItems.isEmpty)
     XCTAssertEqual(readingListItems.first!.title, article.title)
     XCTAssertEqual(readingListItems.first!.contentId, article.id)
   }
 
-  func test_add_shouldIgnoreArticleIfAlreadyInList() {
+  func test_add_shouldIgnoreArticleIfAlreadyInList() throws {
     let otherArticle = Article.createFixture(id: "otherId")
     model.add(article: article)
     model.add(article: otherArticle)
     model.add(article: article)
     model.add(article: otherArticle)
 
-    XCTAssertEqual(model.bookmarkedArticles.count, 2)
+    let readingListItems = try context.fetch(ReadingListItem.fetchRequest(articleIds: [article.id, otherArticle.id]))
+    XCTAssertEqual(readingListItems.count, 2)
   }
 }
