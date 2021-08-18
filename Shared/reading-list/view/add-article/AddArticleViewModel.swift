50020b091169d5e197bb0010e4a277faa18f3c25
Mathias Remshardt <remshams@gmail.com>
2021-08-18 20:26:32 +0200

    feat(AddArticle): adds displaying error message in case article url is invalid

--- Shared/reading-list/view/add-article/AddArticleViewModel.swift
+++ Shared/reading-list/view/add-article/AddArticleViewModel.swift
@@ -5,44 +5,57 @@
 //  Created by Mathias Remshardt on 25.07.21.
 //
 
+import Combine
 import Foundation
 import SwiftUI
-import Combine
 
 typealias AddArticle = (Article) -> Void
 typealias CancelAddArticle = () -> Void
+
+enum AddArticleViewState: Equatable {
+  case initial
+  case articleLoaded(Article)
+  case error(LocalizedStringKey)
+}
 
 class AddArticleViewModel: ObservableObject {
   let addArticle: AddArticle
   let cancelAddArticle: () -> Void
   let getArticle: GetArticle
   var cancellables: Set<AnyCancellable>
-  @Published var article: Article?
+  @Published var state: AddArticleViewState = .initial
 
   init(
     addArticle: @escaping AddArticle,
     cancelAddArticle: @escaping CancelAddArticle,
     getArticle: GetArticle
   ) {
-    self.cancellables = []
+    cancellables = []
     self.addArticle = addArticle
     self.cancelAddArticle = cancelAddArticle
     self.getArticle = getArticle
   }
 
   func loadArticle(for url: String) {
-    if let articleUrl = ArticleUrl(url: url) {
+    if let articleUrl = ArticleUrl(url: url), articleUrl.path != nil {
       getArticle.getBy(url: articleUrl)
-        .replaceError(with: nil)
-        .assign(to: \.article, on: self)
+        .map { article in
+          if let article = article {
+            return AddArticleViewState.articleLoaded(article)
+          } else {
+            return AddArticleViewState.error("Article not found")
+          }
+        }
+        .replaceError(with: AddArticleViewState.error("Article load error"))
+        .assign(to: \.state, on: self)
         .store(in: &cancellables)
     } else {
-      article = nil
+      state = AddArticleViewState.error("Article Url invalid")
     }
   }
 
   func add() {
-    if let article = article {
+    if case let .articleLoaded(article) = state {
       addArticle(article)
     } else {
       cancel()
